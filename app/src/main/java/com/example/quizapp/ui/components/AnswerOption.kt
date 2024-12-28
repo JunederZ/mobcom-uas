5a002ce5308bc5ed5a70bc00cf8afe792deaa6a1
ktsabit <ktsabit@gmail.com>
2024-12-28 18:47:01 +0700

    feat: refactor and add selectable

--- app/src/main/java/com/example/quizapp/ui/components/AnswerOption.kt
+++ app/src/main/java/com/example/quizapp/ui/components/AnswerOption.kt
@@ -2,28 +2,41 @@ package com.example.quizapp.ui.components
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun AnswerCard () {
+fun AnswerOption () {
     Box (
         modifier = Modifier
 //            .padding(16.dp)
-            .border(BorderStroke(2.dp, Color.White))
+            .clip(MaterialTheme.shapes.medium)
+            .border(
+                BorderStroke(1.dp, Color.White),
+                MaterialTheme.shapes.medium
+            )
             .fillMaxWidth()
+            .clickable { println("clicked") }
             .padding(24.dp)
-            ,
+
+        ,
         contentAlignment = Alignment.Center
     ){
         Text("Lorem ipsum dolor sit amet")
     }
+
 }
--- app/src/main/java/com/example/quizapp/ui/screens/QuestionScreen.kt
+++ app/src/main/java/com/example/quizapp/ui/screens/QuestionScreen.kt
@@ -3,43 +3,106 @@ package com.example.quizapp.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.quizapp.ui.components.AnswerCard
+import com.example.quizapp.ui.components.AnswerOption
 
 
 @Composable
 fun QuestionScreen(
     modifier: Modifier = Modifier
 ) {
-    Box (
+    Column (
         modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        contentAlignment = Alignment.TopCenter
-    ) {
-        Column {
-            Text("Question Body: Lorem ipsum dolor sit amet")
-            Spacer(modifier=Modifier.height(16.dp))
-            // answer choice
+            .fillMaxSize()
+//            .background(Color.Red)
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+        ) {
+        Box (
+            modifier = modifier
+                .fillMaxWidth(),
+//                .background(Color.Blue),
+            contentAlignment = Alignment.TopCenter
+        ) {
             Column (
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+            )  {
+                Row (
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.Bottom
+
+                ) {
+                    Text("Quiz A", style = MaterialTheme.typography.headlineMedium)
+                    Text("Question 1 of 4")
+                }
+                Spacer(modifier=Modifier.height(8.dp))
+
+
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth(),
+                    progress = {0.25f}
+                )
+                Spacer(modifier=Modifier.height(16.dp))
+
+
+                Text("Question Body: Lorem ipsum dolor sit amet", style = MaterialTheme.typography.titleMedium)
+                Spacer(modifier=Modifier.height(16.dp))
+                // answer choice
+                Column (
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    AnswerOption()
+                    AnswerOption()
+                    AnswerOption()
+                    AnswerOption()
+                }
+            }
+
+        }
+
+        Row(
+            modifier = Modifier
+//                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconButton(
+                onClick = { /* previous */ }
             ) {
-                AnswerCard()
-                AnswerCard()
-                AnswerCard()
-                AnswerCard()
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "Previous"
+                )
+            }
+
+            IconButton(
+                onClick = { /* next */ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = "Next"
+                )
             }
         }
 
     }
+
 }
