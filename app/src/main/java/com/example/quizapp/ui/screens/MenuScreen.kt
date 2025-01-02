e66d65b5fcaf198ce9e4b4fb1a3df316ce348427
kaisants <contact@kaisan.dev>
2025-01-02 20:43:18 +0700

    feat: icon

--- app/src/main/java/com/example/quizapp/ui/screens/MenuScreen.kt
+++ app/src/main/java/com/example/quizapp/ui/screens/MenuScreen.kt
@@ -1,12 +1,15 @@
 package com.example.quizapp.ui.screens
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,11 +35,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.quizapp.R
 import com.example.quizapp.ui.components.QuizBox
 
 import com.example.quizapp.ui.theme.AppTheme
@@ -72,10 +81,18 @@ fun MainPage(
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    Text(
-                        text = "Quzap",
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.primary
+                    Image(
+                        ImageVector.vectorResource(R.drawable.iconn), "Icon",
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .clip(MaterialTheme.shapes.extraLarge)
+
+                            .width(52.dp)
+                            .height(52.dp)
+                            .background(Color.White)
+                            .padding(8.dp)
+
+
                     )
                 },
                 modifier = Modifier,
@@ -106,7 +123,10 @@ fun MainPage(
                     ) {
                     TextButton(
                         onClick = { viewModel.addNewQuiz() },
-                        modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(100.dp))
+                        modifier = Modifier.background(
+                            MaterialTheme.colorScheme.primaryContainer,
+                            RoundedCornerShape(100.dp)
+                        )
                     ) {
                         Icon(
                             imageVector = Icons.Default.Add,
@@ -117,7 +137,10 @@ fun MainPage(
                     }
                     TextButton(
                         onClick = { showEdit = !showEdit },
-                        modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(100.dp))
+                        modifier = Modifier.background(
+                            MaterialTheme.colorScheme.primaryContainer,
+                            RoundedCornerShape(100.dp)
+                        )
                     ) {
                         Icon(
                             imageVector = Icons.Default.Edit,
